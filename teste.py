ce30a210d3516e38402d5691160aefd83518fcb5
Amanda Rodrigues de Oliveira <oliveira.amanda@ifood.com.br>
2021-10-22 14:57:18 -0300

    amanda teste gitbasic

---
+++ teste.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def buscar_dados():
+    request = requests.get("http://localhost:3002/api/todo")
+    todos = json.loads(request.content)
+    print(todos)
+    print(todos[0]['titulo'])
+
+if __name__ == '__main__':
+    buscar_dados()
+
+
+
+
+#buscardados
